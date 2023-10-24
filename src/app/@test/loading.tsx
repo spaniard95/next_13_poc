e54e0a4aa6223faf54a923a4f9e9ee35b8999363
Panagiotis Spanos <panagiotis.spanos@post.at>
2023-10-24 21:50:47 +0300

    small clean up

--- src/app/@test/loading.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default async function Test() {
-  return <div>Loading</div>;
-}
--- src/app/@test/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default async function Test() {
-  await new Promise((resolve) => setTimeout(resolve, 4000));
-  return <div>Test</div>;
-}
