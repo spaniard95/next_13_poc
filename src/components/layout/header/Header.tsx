304109df072e4d37ce08773acbc0b14470d435d4
spaniard95 <panagiotis.spanos95@gmail.com>
2023-11-05 12:45:56 +0200

    catalog page and drawer

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header className="bg-blue-500 text-white py-4">
+    <header className="bg-blue-500 text-white py-4 px-500">
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
           <h1 className="text-2xl font-bold">My App</h1>
           <nav className="flex-row space-x-4">
             <Link href="/">Home</Link>
             <Link href="/dashboard/page1">Dashboard</Link>
+            <Link href="/catalog">Catalog</Link>
           </nav>
         </div>
       </div>
