e734480c6f592e8a6f487e911496e79f5485af59
spaniard95 <panagiotis.spanos95@gmail.com>
2023-11-05 19:23:37 +0200

    small drawer reafactor

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -5,14 +5,16 @@ import React, { useState } from "react";
 export default function Catalog() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   return (
-    <div className="relative display-flex" style={{ height: 1000 }}>
+    <div
+      className="relative display-flex"
+      style={{ minHeight: 2000, maxHeight: 2400 }}
+    >
       <button
         onClick={() => setIsDrawerOpen(!isDrawerOpen)}
         className="absolute top-0 left-0 mt-4 ml-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 focus:outline-none"
       >
         {isDrawerOpen ? "Close" : "Open"} Drawer
       </button>
-
       <CatalogDrawer open={isDrawerOpen} />
     </div>
   );
--- src/components/catalog-drawer/CatalogDrawer.tsx
+++ src/components/catalog-drawer/CatalogDrawer.tsx
@@ -7,15 +7,17 @@ interface CatalogDrawerProps {
   open: boolean;
 }
 
-const CatalogDrawer: React.FC<CatalogDrawerProps> = ({ open }) => {
+const CatalogDrawer: React.FC<CatalogDrawerProps> = ({ open = true }) => {
   return (
-    <div
-      className={`absolute top-0 right-0 w-64 bg-gray-200 p-4 transition-transform transform ${
-        open ? "translate-x-0" : "translate-x-full"
-      } h-full shadow-lg z-10`}
-    >
-      Drawer Content
-    </div>
+    open && (
+      <div
+        className={`absolute top-0 right-0 w-64 bg-gray-200 p-4 transition-transform transform ${
+          open ? "translate-x-0" : "translate-x-full"
+        } h-full shadow-lg z-10`}
+      >
+        Drawer Content
+      </div>
+    )
   );
 };
 
