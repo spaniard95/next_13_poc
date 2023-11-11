ff1fb334353d4a44d7ebcd2df4a7b1fc8f75140a
spaniard95 <panagiotis.spanos95@gmail.com>
2023-11-11 21:42:22 +0200

    fix button alignment

--- src/components/catalog-drawer/CatalogDrawer.tsx
+++ src/components/catalog-drawer/CatalogDrawer.tsx
@@ -15,7 +15,9 @@ export default async function CatalogDrawer({ lessonId }: CatalogDrawerProps) {
       className={`absolute top-0 right-0 w-1/3 bg-gray-200 p-4 transition-transform transform translate-x-0" h-full shadow-lg z-10`}
     >
       {lesson.title}
-      <NavigationButton path="/catalog" label="Back" />
+      <div>
+        <NavigationButton path="/catalog" label="Back" />
+      </div>
     </div>
   );
 }
